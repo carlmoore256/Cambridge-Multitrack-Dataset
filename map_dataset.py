87f7d790c1ae8427c6c9773491f40444fb6dd33a
unknown <carlmoore256@gmail.com>
2021-04-05 18:58:24 -0700

    fixed issue with keywords not matching in kw_filt

--- map_dataset.py
+++ map_dataset.py
@@ -41,17 +41,28 @@ def extract_clips(path, silence_thresh, ws=2048, hop=1024, min_len=4096):
                                                 min_len=min_len)
     return clips, intervals, num_samps
 
-def create_map(data_path, kw_path, conf_thresh, silence_thresh, n_jobs=8):
+# main func for generating complete map with AudioSet classes and matched keywords (see keywords.txt)
+# if a dataset map is provided, the entries will be modified in place, overwriting existing entries
+# if overwrite=True; save_interval saves the map every n sessions for convenience
+def create_map(data_path, kw_path, conf_thresh, silence_thresh, n_jobs=8, dir_map={}, 
+               outpath="complete_map.json", overwrite=True, save_interval=5):
     yamnet = Yamnet()
     keywords = file_utils.load_keywords(kw_path)
     kw_filt = extract_labels.FilterStems(keywords, conf_thresh)
-    dir_map = {}
+
+    existing_keys = dir_map.keys()
+    analyzed = 0
 
     for root, dirs, files in os.walk(data_path):
         if len(dirs) > 0:
             session_name = get_session_name(dirs)
 
-        if session_name is not None and len(files) > 0:
+        write_entry = True
+
+        if session_name is not None and overwrite == False and session_name in existing_keys:
+            write_entry = False
+
+        if session_name is not None and len(files) > 0 and write_entry:
             dir_map[session_name] = {}
             # verify the files are valid ones
             valid_files = [os.path.abspath(os.path.join(root, f)) for f in files if is_valid_file(f)]
@@ -62,10 +73,7 @@ def create_map(data_path, kw_path, conf_thresh, silence_thresh, n_jobs=8):
             print(f"calculating features for {len(valid_files)} tracks in {session_name}")
 
             for i, (clips, intervals, num_samps) in enumerate(extracted_clips):
-                f = valid_files[i]
-
-                full_path = os.path.join(root, f)
-                full_path = os.path.abspath(full_path)
+                full_path = valid_files[i]
 
                 # REMOVE SILENCE BEFORE YAMNET PROCESSING
                 # clips, intervals, num_samps = extract_clips(full_path,
@@ -93,15 +101,22 @@ def create_map(data_path, kw_path, conf_thresh, silence_thresh, n_jobs=8):
                     corrected_intervals.append([int(interval[0]), int(interval[1])])
                     corrected_num_samps += int(interval[1]) - int(interval[0])
                 
-                # correct num_samps as well
-                print(f'file {f} - found {corrected_num_samps} samples matching audioset classes')
-                track = f[:-4] # remove ".wav" from name
+                track = os.path.splitext(os.path.basename(full_path))[0] # remove full path and ext .wav
+
+                print(f'{track} - {corrected_num_samps} samples matching audioset classes')
+
                 dir_map[session_name][track] = {}
                 dir_map[session_name][track]["path"] = str(full_path)
-                dir_map[session_name][track]["keywords"] = list(kw_filt.filter(f))
+                dir_map[session_name][track]["keywords"] = list(kw_filt.filter(track))
                 dir_map[session_name][track]["numsamps"] = int(num_samps)
                 dir_map[session_name][track]["intervals"] = list(corrected_intervals)
                 dir_map[session_name][track]["audioset"] = audioset_classes
+
+            analyzed += 1
+
+            if analyzed % save_interval == save_interval - 1:
+                print("save interval reached, saving map...")
+                file_utils.save_json(outpath, dir_map, indent=2)
 
     return dir_map
 
@@ -116,8 +131,12 @@ if __name__ == "__main__":
         help="path to downloaded mutlitracks")
     parser.add_argument("--out", type=str, default="./complete_map.json", 
         help="output path of verified map")
-    # parser.add_argument("--map", type=str, default="dataset_map.json", 
-    #     help="path do json dataset map, if it does not exist one will be created")
+    parser.add_argument("--map", type=str, default=None, 
+        help="path do existing dataset map which, if specified, will be modified in place")
+    parser.add_argument("--overwrite", type=bool, default=True, 
+        help="overwrite existing entries if existing dataset map is loaded")
+    parser.add_argument("--save_interval", type=int, default=5, 
+        help="how often to save the dataset map while processing tracks")
     parser.add_argument("-kw", type=str, default="keywords.txt",
         help="keywords txt file that specifies search terms")
     parser.add_argument("--c_thresh", type=int, default=80,
@@ -128,7 +147,12 @@ if __name__ == "__main__":
         help="num parallel worker threads to load & process audio files")
 
     args = parser.parse_args()
-    
-    dataset_map = create_map(args.path, args.kw, args.c_thresh, args.thresh_db, args.n_jobs)
+
+    dataset_map = {} # initialzie empty dict
+    if args.map is not None: # if existing map is provided, it will be modified in place (overwriting existing entries)
+        dataset_map = file_utils.load_json(args.map)
+
+    dataset_map = create_map(args.path, args.kw, args.c_thresh, args.thresh_db, args.n_jobs, dataset_map, 
+                            args.out, args.overwrite, args.save_interval)
 
     file_utils.save_json(args.out, dataset_map, indent=2)
